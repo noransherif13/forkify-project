ce7230a94632a363e2fc8da5f0618d5daa67c80a
Noran <noranosherif958@gmail.com>
2023-08-20 18:22:40 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '602d06a4-5920-4f4b-bc5c-0c657668464c';
+export const MODAL_CLOSE_SEC = 2.5;
